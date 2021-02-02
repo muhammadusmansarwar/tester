befc2ca926ec508229103a28948ebbe1b3f26b56
muhammad usman sarwar <muhammadusmanserwer@yahoo.com>
2021-02-02 23:44:22 +0500

    this is first push

---
+++ explode.php
@@ -0,0 +1,16 @@
+<?php
+echo "<pre>";
+$v = "
+Lipsum generator: Lorem Ipsum - All the factswww.lipsum.com
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when ...
+
+Lorem Ipsum – Generator, Origins and Meaningloremipsum.io
+Generate Lorem Ipsum placeholder text for use in your graphic, print and web ... In seeing a sample of lorem ipsum, his interest was piqued by consectetur—a ...
+‎Images · ‎Plugins · ‎Generators · ‎English
+
+Best Text-to-Speech Demo: Create Talking Avatars and ...ttsdemo.com
+Try SitePal's talking avatars with our free Text to Speech online demo. Our virtual characters read text aloud naturally in over 25 languages. Use our text to ...";
+$var = explode(PHP_EOL, $v);
+print_r($var);
+
+?>
---
+++ if.php
@@ -0,0 +1,14 @@
+<?php
+
+//True = 1
+//False = 0
+$ar = array('MUS');
+$v = 4334;
+if ("1") {
+	echo "True";
+}
+else {
+	echo "False";
+}
+
+?>
